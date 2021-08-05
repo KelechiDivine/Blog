0598669ccfaceabcfb75877e32c714bbc5d13b5f
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-08-05 13:47:25 +0100

    origin main

--- src/main/java/com/demonblog/BlogApplication.java
+++ src/main/java/com/demonblog/BlogApplication.java
@@ -8,6 +8,6 @@ public class BlogApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
+		System.out.println("Blog post finished running.");
 	}
-	
 }
