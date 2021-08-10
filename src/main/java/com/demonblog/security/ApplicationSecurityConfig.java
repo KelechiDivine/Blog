2b5ecd4b9a8c0f87d8f8901e54e5e7198c53b292
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-08-10 13:17:58 +0100

    refactored pom.xml file

--- src/main/java/com/demonblog/security/ApplicationSecurityConfig.java
+++ src/main/java/com/demonblog/security/ApplicationSecurityConfig.java
@@ -1,23 +1,42 @@
 package com.demonblog.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	private final PasswordEncoder passwordEncoder;
+	
+	
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder1){
+		this.passwordEncoder = passwordEncoder1;
+	}
 	// press command 'o' to generate this userDetailsService
 	
 	@Override
 	@Bean
 	protected UserDetailsService userDetailsService() {
-		return super.userDetailsService();
+		UserDetails firstUser = User.builder()
+				.username("Kelechi")
+				.password("password")
+				.roles("Blog_User") // ROLES_BLOG_USER
+				.build();
+		
+		return new InMemoryUserDetailsManager(
+				firstUser
+		);
 	}
 	
 	@Override
